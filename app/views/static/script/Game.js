18a1af333f8afb9da423c21a89407f97a6af67e7
gitviini <gvinicius105@gmail.com>
2025-02-21 23:10:08 -0300

    container menu upload changes

--- app/views/static/script/Game.js
+++
@@ -1,6 +0,0 @@
-const toggleMenu = document.querySelector(".toggleMenu")
-const closeMenu = document.querySelector(".closeMenu")
-const containerMenu = document.querySelector(".container_menu")
-
-toggleMenu.onclick = () => containerMenu.classList.add("open")
-closeMenu.onclick = () => containerMenu.classList.remove("open")
--- app/views/static/script/base.js
+++ app/views/static/script/base.js
@@ -1,3 +1,14 @@
 //* ColorScheme Prefer User 
 const colorScheme = window.matchMedia('(prefers-color-scheme: dark)')
 colorScheme ? document.body.classList.add("dark") : document.body.classList.remove("dark")
+
+//* Menu
+try{
+    const toggleMenu = document.querySelector(".toggleMenu")
+    const closeMenu = document.querySelector(".closeMenu")
+    const containerMenu = document.querySelector(".containerMenu")
+    
+    toggleMenu.onclick = () => containerMenu.classList.add("open")
+    closeMenu.onclick = () => containerMenu.classList.remove("open")
+}
+catch(err){}
