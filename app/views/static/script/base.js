7148c6182f615f74da486d41e68ffbd113f10701
Vinicius Gabriel <146395205+gitviini@users.noreply.github.com>
2025-02-22 02:49:09 +0000

    base css upload

--- app/views/static/script/base.js
+++ app/views/static/script/base.js
@@ -1,6 +1,8 @@
 //* ColorScheme Prefer User 
 const colorScheme = window.matchMedia('(prefers-color-scheme: dark)')
-colorScheme ? document.body.classList.add("dark") : document.body.classList.remove("dark")
+colorScheme.matches ? document.body.classList.add("dark") : document.body.classList.remove("dark")
+
+console.log(colorScheme)
 
 //* Menu
 try{
