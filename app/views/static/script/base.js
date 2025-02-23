826213fc1cf241595b0041fc0e3f5376aa906f84
gitviini <gvinicius105@gmail.com>
2025-02-23 11:51:24 -0300

    system of detect changes in theme

--- app/views/static/script/base.js
+++ app/views/static/script/base.js
@@ -1,21 +1,29 @@
 //* ColorScheme Prefer User
-const colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-colorScheme ? document.body.classList.add("dark") : document.body.classList.remove("dark")
+// Handler function to set colorscheme
+function handlerColorScheme(colorScheme) { 
+    colorScheme ? document.body.classList.add("dark") : document.body.classList.remove("dark") 
+}
+// Instance of window.matchMedia
+const colorSchemeInstance = window.matchMedia('(prefers-color-scheme: dark)')
+// First set colorscheme and detect change of system
+handlerColorScheme(colorSchemeInstance.matches)
+colorSchemeInstance.onchange = (event) => handlerColorScheme(event.matches)
+
 
 //* Logo configure
-try{
+try {
     const logo = document.querySelector(".logo")
     colorScheme ? logo.src = "/img/dark.png" : logo.src = "/img/light.png"
-}catch(err){}
+} catch (err) { }
 
 //* Menu
-try{
+try {
     const toggleMenu = document.querySelector(".toggleMenu")
     const closeMenu = document.querySelector(".closeMenu")
     const containerMenu = document.querySelector(".containerMenu")
-    
+
     toggleMenu.onclick = () => containerMenu.classList.add("open")
     closeMenu.onclick = () => containerMenu.classList.remove("open")
     containerMenu.onmouseleave = () => containerMenu.classList.remove("open")
 }
-catch(err){}
+catch (err) { }
